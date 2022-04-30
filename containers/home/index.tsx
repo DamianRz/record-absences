43922724b7072a581854e7e164527ae8390b4b62
Damian Rodriguez <drodriguez@vairix.com>
2022-04-30 11:26:35 -0300

    add base files

---
+++ containers/home/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+   <div className='w-screen h-screen bg-cyan-500'>
+   </div>
+  )
+}
+
+export default Home
---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>RecordAbsences</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="w-screen h-screen bg-cyan-500">
+      </main>
+    </>
+  )
+}
+
+export default Index
