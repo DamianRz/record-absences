4db67ca58a1e3d775293345c7153f0f9233d9025
Damian Rodriguez <drodriguez@vairix.com>
2022-08-22 22:06:47 -0300

    fix structure of the drawer

--- components/drawer/drawer.tsx
+++ components/drawer/drawer.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./drawer.module.scss";
+import EventBusyIcon from '@mui/icons-material/EventBusy';
+import GroupIcon from '@mui/icons-material/Group';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import PersonIcon from '@mui/icons-material/Person';
 
 interface DrawerProps {
     isAdmin: boolean
@@ -7,15 +12,44 @@ interface DrawerProps {
 
 const tools = [
     {
-        label: "",
-        name: "d",
-        icon: "s"
+        label: "Inasistencias",
+        icon: EventBusyIcon
+    },
+    {
+        label: "Grupos",
+        icon: GroupIcon
+    },
+    {
+        label: "Materias",
+        icon: AssignmentIcon
+    },
+    {
+        label: "Reportes",
+        icon: AssessmentIcon
+    },
+    {
+        label: "Usuarios",
+        icon: PersonIcon
     }
 ];
 
 const Drawer: React.FC<DrawerProps> = ({ isAdmin }) => {
+    const [selected, setSelected] = useState(0);
     return (
         <div className={styles.drawer}>
+            <p className={styles.title}>Herramientas</p>
+            <ul>
+                { tools.map((item, index) => 
+                    <li 
+                        key={index} 
+                        className={selected === index ? styles.selected : ""}
+                        onClick={()=> { setSelected(index) }}
+                    >
+                        <item.icon />
+                        {item.label}
+                    </li>
+                )}
+            </ul>
         </div>
     );
 };
