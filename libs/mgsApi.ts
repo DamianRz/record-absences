4f454c13194b8fe58e5efa3f64c98f44bbadfeb6
Damian Rodriguez <damianezetiel1@gmail.com>
2023-01-14 02:50:06 -0300

    add api functions in mgsApi

---
+++ libs/mgsApi.ts
@@ -0,0 +1,14 @@
+export const getMgs = async (mgId: number): Promise<any> => {
+    const apiUrl = process.env.API_URL || 'http://26.80.200.141:3000'
+    const token = localStorage.getItem('token');
+    return await fetch(`${apiUrl}/mgs/${mgId}/all`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Authorization': `Bearer ${token}`
+        },
+    }).then(response => response.json());
+}
