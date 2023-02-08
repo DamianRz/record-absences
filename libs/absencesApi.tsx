4d5b49ea9e1d191043aff35da97b617b536d66d5
Damian Rodriguez <damianezetiel1@gmail.com>
2023-02-07 22:59:05 -0300

    add save api in absences

--- libs/absencesApi.tsx
+++ libs/absencesApi.tsx
@@ -13,11 +13,27 @@ export const getAbsences = async (): Promise<any> => {
     }).then(response => response.json())
 }
 
-export const saveAbsence = async (absenceData: any): Promise<any> => {
+export const createAbsence = async (absenceData: any): Promise<any> => {
     const apiUrl = process.env.API_URL || 'http://localhost:3000'
     const token = localStorage.getItem('token');
     return await fetch(`${apiUrl}/absences/create`, {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ ...absenceData })
+    }).then(response => response.json())
+}
+
+export const saveAbsence = async (absenceId: number, absenceData: any): Promise<any> => {
+    const apiUrl = process.env.API_URL || 'http://localhost:3000'
+    const token = localStorage.getItem('token');
+    return await fetch(`${apiUrl}/absences/${absenceId}`, {
+        method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
