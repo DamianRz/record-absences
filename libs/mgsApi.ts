46dfdde0e8fffc48e9484007100c098f4fe4df9a
Damian Rodriguez <damianezetiel1@gmail.com>
2023-02-12 19:20:18 -0300

    add new api functions in mgsApi

--- libs/mgsApi.ts
+++ libs/mgsApi.ts
@@ -28,3 +28,50 @@ export const getMgs = async (filters: any): Promise<any> => {
         body: JSON.stringify({ ...filters })
     }).then(response => response.json());
 }
+
+export const createMg = async (mg: any): Promise<any> => {
+    const apiUrl = process.env.API_URL || 'http://localhost:3000'
+    const token = localStorage.getItem('token');
+    return await fetch(`${apiUrl}/mgs/create`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ ...mg })
+    }).then(response => response.json());
+}
+
+export const saveMg = async (mgId: number, mg: any): Promise<any> => {
+    const apiUrl = process.env.API_URL || 'http://localhost:3000'
+    const token = localStorage.getItem('token');
+    return await fetch(`${apiUrl}/mgs/${mgId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'PATCH',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ ...mg })
+    }).then(response => response.json());
+}
+
+export const deleteMg = async (mgId: number): Promise<any> => {
+    const apiUrl = process.env.API_URL || 'http://localhost:3000'
+    const token = localStorage.getItem('token');
+    return await fetch(`${apiUrl}/mgs/${mgId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Authorization': `Bearer ${token}`
+        }
+    }).then(response => response.json());
+}
